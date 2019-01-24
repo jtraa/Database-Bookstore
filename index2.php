663f96b084695974ad3268fcc2b32b833e496587
Jelle Traa <jelletraa@gmail.com>
2019-01-24 16:32:22 +0100

    update 163224012019

--- index2.php
+++ index2.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">        
-        <link rel="stylesheet" type="text/css" media="screen" href="styletest.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="styletest2.css" />
         <link href="https://fonts.googleapis.com/css?family=Crimson+Text|Julius+Sans+One|Oswald" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
@@ -15,7 +15,7 @@
         <div class="allescontainer debug">
             <div class="header debug">My HP Bookstore</div>
                 <div class="cards debug">
-                    <?php include "card.php"; ?>       
+                    <?php include "page2.php"; ?>       
                 </div>
                 <!--
                 <div class="debug footer"></div>
--- page2.php
+++ page2.php
@@ -6,7 +6,7 @@
     include "db_connection.php";
 
 
-    $sql_querie = "SELECT hp_id, hp_title, hp_genre, hp_age, hp_release, hp_price, hp_image, hp_image2 FROM hp";
+    $sql_querie = "SELECT harry_id, hp_publisher, hp_writer, hp_information, hp_image, hp_image2 FROM hp2";
     
     $db_result = $conn->query($sql_querie);  
 
@@ -14,17 +14,17 @@
     {           
         echo '<div class="card debug">' .
              '<div class="h1">' .
-                '<h2>' . $row['hp_title'] . '</h2>' . 
+                '<h2>' . $row['hp_writer'] . '</h2>' . 
              '</div>' .
              '<div class="Hoi">' .
             '<div class="flip-card">' .
                 '<div class="flip-card-inner">' .
                     '<div class="flip-card-front">' .
-                       '<a href= "index2.php">' . '<img src="' . $row['hp_image'] . '" alt="' . $row['hp_title'] . '" style=width:100%">' .
+                       '<a href= "index.php">' . '<img src="' . $row['hp_image'] . '" alt="' . $row['hp_publisher'] . '" style=width:100%">' .
                         //<img src=img\halfbloodprince.jpg style=width:100%>
                     '</div>' .
                     '<div class="flip-card-back">' .
-                    '<img src="' . $row['hp_image2'] . '" alt="' . $row['hp_title'] . '" style=width:100%">' .
+                    '<img src="' . $row['hp_image2'] . '" alt="' . $row['hp_publisher'] . '" style=width:100%">' .
                         //<img src=img\halfbloodprince2.jpg style=width:100%>
                     '</div>' .
                     '</div>'.
@@ -32,7 +32,7 @@
             '</div>'.
         '</div>'.
         '<div class="h2">' .
-            '<h3>'. $row['hp_genre'] . '<br>' . $row['hp_age'] . '+' . '<br>' . '€' . $row['hp_price'] . ',-' . '<br>' . '</h3>' .
+            '<h3>'. $row['hp_information'] . '<br>' . $row['hp_information'] . '+' . '<br>' . '€' . $row['hp_information'] . ',-' . '<br>' . '</h3>' .
          '</div>' .
     '</div>';
 
